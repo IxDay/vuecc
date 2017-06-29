2e4c764b9aa9e524afb0b0b6e2fc1afef5f7e1d4
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-29 23:23:48 +0200

    Now use commander to parse cli

--- index.js
+++ index.js
@@ -3,78 +3,15 @@
 var fs = require('fs');
 var path = require('path');
 var htmlparser = require('html-minifier/src/htmlparser');
+var program = require('commander');
 
-var binName = 'vuec';
-var help = (binName + ' [-o OUTPUT] INPUT...\n\n' +
-  binName + ' compile Vue Single File Components to an OUTPUT javascript file.\n'
-);
-var states = {
-  'VUEC': 0,
-  'OPTIONS': 1,
-  'OUTPUT': 2,
-  'INPUTS': 3,
-};
+(program
+  .version('0.1.0')
+  .description('Compile Vue Single File Components to a javascript file')
+  .option('-o, --output', 'javascript output file, if not specified try to infer it')
+  .parse(process.argv)
+)
 
-var mixedArgsError = 'mixed arguments.';
-var noOutputError = 'no output has been provided and none can be infered.';
-var noInputError = 'no input has been provided.';
-var multipleOutputError = 'output has been provided multiple time.';
-var emptyArgError = 'empty arguments are not supported.';
-var invalidState = 'state machine entered an invalid state.';
-
-function parseArg(res, state, arg) {
-  if (!arg.length) throw emptyArg;
-
-  switch (state) {
-    case states.VUEC:
-      var splitted = arg.split(path.sep);
-      if (splitted[splitted.length-1] == binName) state += 1;
-      break;
-    case states.OPTIONS:
-      if (arg[0] != '-') return parseArg(res, state + 2, arg);
-      if (arg == '-h' || arg == '--help') throw help;
-      if (arg.slice(0, '--output='.length) == '--output=')
-        return parseArg(res, state + 1, arg.slice('--output='.length));
-
-      if (arg == '-o' || arg == '--output') state += 1;
-      break;
-    case states.OUTPUT:
-      if (res.inputs.length != 0) throw mixedArgsError;
-      if (res.output != null) throw multipleOutputError;
-      res.output = arg;
-      state -= 1;
-      break;
-    case states.INPUTS:
-      if (arg[0] == '-') throw mixedArgsError;
-      res.inputs.push(arg)
-      break;
-  }
-  return state;
-}
-
-function parseArgs() {
-  var state = states.VUEC;
-  var res = {
-    'output': null,
-    'inputs': []
-  };
-
-  process.argv.forEach(function(arg) {
-    state = parseArg(res, state, arg);
-  });
-
-  if (res.inputs.length == 0) throw noInputError;
-  if (res.output == null) {
-    if (res.inputs.length > 1) throw noOutputError;
-    var output = res.inputs[0].split('.')
-    res.output = (
-      output.length > 1
-      ? output.slice(0, output.length -1).join('')
-      : output[0]
-    ) + '.js';
-  }
-  return res;
-}
 function parse(data) {
   var ctx = {};
   htmlparser.HTMLParser(data, {
@@ -114,26 +51,23 @@ function parse(data) {
 }
 
 function compile(context) {
-  var stream = fs.createWriteStream(context.output, {encoding: 'utf8'});
-
-  stream.once('open', function(_) {
-    context.inputs.forEach(function(input) {
+  var stream;
+  var cb = function() {
+    context.args.forEach(function(input) {
       fs.readFile(input, {encoding: 'utf8'}, function (err, data) {
         if (err) throw err;
-        var ctx = parse(data);
-        console.log(ctx);
+        stream.write(parse(data).template);
       });
     });
-  });
+  }
+  if (context.output) {
+    stream = fs.createWriteStream(context.output, {encoding: 'utf8'});
+    stream.once('open', cb);
+  } else {
+    stream = process.stdout;
+    cb();
+  }
+
 }
 
-try {
-  compile(parseArgs());
-} catch (e) {
-  if (e == help) {
-    console.log(help);
-  } else {
-    console.error('ERROR:', e, '\n');
-    console.error(help);
-  }
-}
+compile(program);
