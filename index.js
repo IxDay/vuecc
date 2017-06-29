77d15eb5adb3eabebd016900ad4a458e299980c1
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-29 22:49:01 +0200

    Working parser!

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var noOutputError = "no output has been provided and none can be infered.";
 var noInputError = "no input has been provided.";
 var multipleOutputError = "output has been provided multiple time.";
 var emptyArgError = "empty arguments are not supported.";
+var invalidState = "state machine entered an invalid state.";
 
 function parseArg(res, state, arg) {
   if (!arg.length) throw emptyArg;
@@ -26,20 +27,15 @@ function parseArg(res, state, arg) {
   switch (state) {
     case states.VUEC:
       var splitted = arg.split(path.sep);
-      if (splitted[splitted.length-1] == binName) {
-        state += 1;
-      }
+      if (splitted[splitted.length-1] == binName) state += 1;
       break;
     case states.OPTIONS:
-      if (arg[0] != "-") {
-        parseArg(res, state + 2, arg);
-      } else if (arg == "-h" || arg == "--help") {
-        throw help;
-      } else if (arg == "-o" || arg == "--output") {
-        state += 1;
-      } else if (arg.slice(0, "--output=".length) == "--output=") {
-        parseArg(res, state + 1, arg.slice("--output=".length));
-      }
+      if (arg[0] != "-") return parseArg(res, state + 2, arg);
+      if (arg == "-h" || arg == "--help") throw help;
+      if (arg.slice(0, "--output=".length) == "--output=")
+        return parseArg(res, state + 1, arg.slice("--output=".length));
+
+      if (arg == "-o" || arg == "--output") state += 1;
       break;
     case states.OUTPUT:
       if (res.inputs.length != 0) throw mixedArgsError;
@@ -79,8 +75,115 @@ function parseArgs() {
   return res;
 }
 
+var invalidTemplateError = function (char) {
+  return "template is invalid, encounter an invalid char: " + char;
+}
+var spacechars = [" ", "\t", "\r", "\n", "\f", "\v"];
+
+function Handler() {
+  this.state = null;
+  this.nodes = {};
+
+  this.start = function (buf) {
+    var state = buf.slice(1, buf.length-1).join("");
+    if (state == "script" || state == "template") {
+      this.state = state;
+      return true;
+    }
+  }
+  this.end = function (buf) {
+    return buf.slice(2, buf.length-1).join("") == this.state
+  }
+  this.body = function (buf) {
+    this.nodes[this.state] = buf.join("").trim();
+  }
+}
+
+
+function expect(char, expected) {
+  return char == expected;
+}
+
+function expects(char, chars) {
+  if (!chars.length) return;
+  return expect(char, chars[0]) || expects(char, chars.slice(1));
+}
+
+function parse(chunk, handler) {
+  var buf = [], tag = [];
+  var states = {
+    "OUT": 0,
+    "ENTERINGCLOSING": 1,
+    "ENTERING": 2,
+    "BODY": 3,
+    "CLOSING": 4
+  };
+  var state = states.OUT;
+
+  chunk.split("").forEach(function(char) {
+    switch (state) {
+      case states.OUT:
+        if (expects(char, spacechars)) break;
+        if (!expect(char, "<")) throw invalidTemplateError(char);
+
+        state = states.ENTERINGCLOSING;
+        tag.push(char);
+        break;
+      case states.ENTERINGCLOSING:
+        state = expect(char, "/") ? states.CLOSING : states.ENTERING;
+        tag.push(char);
+        break;
+      case states.ENTERING:
+        tag.push(char);
+
+        if (!expect(char, ">")) break;
+        if (!handler.start(tag)) buf.push.apply(buf, tag);
+
+        tag = [];
+        state = states.BODY;
+        break;
+      case states.BODY:
+        if (expect(char, "<")) {
+          tag.push(char);
+          state = states.ENTERINGCLOSING;
+        } else {
+          buf.push(char);
+        }
+        break;
+      case states.CLOSING:
+        tag.push(char);
+
+        if (!expect(char, ">")) break;
+        if (handler.end(tag)) {
+          handler.body(buf);
+          buf = [];
+          state = states.OUT;
+        } else {
+          buf.push.apply(buf, tag);
+          state = states.BODY;
+        }
+        tag = [];
+        break;
+      default:
+        throw invalidState;
+    }
+  });
+}
+
 function compile(context) {
-  console.log(context);
+  var stream = fs.createWriteStream(context.output, {encoding: "utf8"});
+  stream.once('open', function(_) {
+    context.inputs.forEach(function(input) {
+      fs.readFile(input, {encoding: "utf8"}, function (err, data) {
+        if (err) throw err;
+
+        var handler = new Handler();
+        parse(data, handler);
+        console.log(handler.nodes);
+//        stream.write(data);
+      });
+    });
+  });
 }
 
 try {
