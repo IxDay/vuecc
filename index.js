cbabf1b96baa2ad2582bf696f50c886de3215819
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-29 23:02:41 +0200

    Use html-minifier parser

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 var fs = require('fs');
 var path = require('path');
+var htmlparser = require('html-minifier/src/htmlparser');
 
-var binName = "vuec";
-var help = (binName + " [-o OUTPUT] INPUT...\n\n" +
-  binName + " compile Vue Single File Components to an OUTPUT javascript file.\n"
+var binName = 'vuec';
+var help = (binName + ' [-o OUTPUT] INPUT...\n\n' +
+  binName + ' compile Vue Single File Components to an OUTPUT javascript file.\n'
 );
 var states = {
-  "VUEC": 0,
-  "OPTIONS": 1,
-  "OUTPUT": 2,
-  "INPUTS": 3,
+  'VUEC': 0,
+  'OPTIONS': 1,
+  'OUTPUT': 2,
+  'INPUTS': 3,
 };
 
-var mixedArgsError = "mixed arguments.";
-var noOutputError = "no output has been provided and none can be infered.";
-var noInputError = "no input has been provided.";
-var multipleOutputError = "output has been provided multiple time.";
-var emptyArgError = "empty arguments are not supported.";
-var invalidState = "state machine entered an invalid state.";
+var mixedArgsError = 'mixed arguments.';
+var noOutputError = 'no output has been provided and none can be infered.';
+var noInputError = 'no input has been provided.';
+var multipleOutputError = 'output has been provided multiple time.';
+var emptyArgError = 'empty arguments are not supported.';
+var invalidState = 'state machine entered an invalid state.';
 
 function parseArg(res, state, arg) {
   if (!arg.length) throw emptyArg;
@@ -30,12 +31,12 @@ function parseArg(res, state, arg) {
       if (splitted[splitted.length-1] == binName) state += 1;
       break;
     case states.OPTIONS:
-      if (arg[0] != "-") return parseArg(res, state + 2, arg);
-      if (arg == "-h" || arg == "--help") throw help;
-      if (arg.slice(0, "--output=".length) == "--output=")
-        return parseArg(res, state + 1, arg.slice("--output=".length));
+      if (arg[0] != '-') return parseArg(res, state + 2, arg);
+      if (arg == '-h' || arg == '--help') throw help;
+      if (arg.slice(0, '--output='.length) == '--output=')
+        return parseArg(res, state + 1, arg.slice('--output='.length));
 
-      if (arg == "-o" || arg == "--output") state += 1;
+      if (arg == '-o' || arg == '--output') state += 1;
       break;
     case states.OUTPUT:
       if (res.inputs.length != 0) throw mixedArgsError;
@@ -44,7 +45,7 @@ function parseArg(res, state, arg) {
       state -= 1;
       break;
     case states.INPUTS:
-      if (arg[0] == "-") throw mixedArgsError;
+      if (arg[0] == '-') throw mixedArgsError;
       res.inputs.push(arg)
       break;
   }
@@ -54,8 +55,8 @@ function parseArg(res, state, arg) {
 function parseArgs() {
   var state = states.VUEC;
   var res = {
-    "output": null,
-    "inputs": []
+    'output': null,
+    'inputs': []
   };
 
   process.argv.forEach(function(arg) {
@@ -65,122 +66,62 @@ function parseArgs() {
   if (res.inputs.length == 0) throw noInputError;
   if (res.output == null) {
     if (res.inputs.length > 1) throw noOutputError;
-    var output = res.inputs[0].split(".")
+    var output = res.inputs[0].split('.')
     res.output = (
       output.length > 1
       ? output.slice(0, output.length -1).join('')
       : output[0]
-    ) + ".js";
+    ) + '.js';
   }
   return res;
 }
-
-var invalidTemplateError = function (char) {
-  return "template is invalid, encounter an invalid char: " + char;
-}
-var spacechars = [" ", "\t", "\r", "\n", "\f", "\v"];
-
-function Handler() {
-  this.state = null;
-  this.nodes = {};
-
-  this.start = function (buf) {
-    var state = buf.slice(1, buf.length-1).join("");
-    if (state == "script" || state == "template") {
-      this.state = state;
-      return true;
-    }
-  }
-  this.end = function (buf) {
-    return buf.slice(2, buf.length-1).join("") == this.state
-  }
-  this.body = function (buf) {
-    this.nodes[this.state] = buf.join("").trim();
-  }
-}
-
-
-function expect(char, expected) {
-  return char == expected;
-}
-
-function expects(char, chars) {
-  if (!chars.length) return;
-  return expect(char, chars[0]) || expects(char, chars.slice(1));
-}
-
-function parse(chunk, handler) {
-  var buf = [], tag = [];
-  var states = {
-    "OUT": 0,
-    "ENTERINGCLOSING": 1,
-    "ENTERING": 2,
-    "BODY": 3,
-    "CLOSING": 4
-  };
-  var state = states.OUT;
-
-  chunk.split("").forEach(function(char) {
-    switch (state) {
-      case states.OUT:
-        if (expects(char, spacechars)) break;
-        if (!expect(char, "<")) throw invalidTemplateError(char);
-
-        state = states.ENTERINGCLOSING;
-        tag.push(char);
-        break;
-      case states.ENTERINGCLOSING:
-        state = expect(char, "/") ? states.CLOSING : states.ENTERING;
-        tag.push(char);
-        break;
-      case states.ENTERING:
-        tag.push(char);
-
-        if (!expect(char, ">")) break;
-        if (!handler.start(tag)) buf.push.apply(buf, tag);
-
-        tag = [];
-        state = states.BODY;
-        break;
-      case states.BODY:
-        if (expect(char, "<")) {
-          tag.push(char);
-          state = states.ENTERINGCLOSING;
-        } else {
-          buf.push(char);
+function parse(data) {
+  var ctx = {};
+  htmlparser.HTMLParser(data, {
+    state: null,
+    buf: [],
+    states: ['template', 'script'],
+    start: function(tag, attrs, unary) {
+      for (var i = 0; i < this.states.length; i++) {
+        if (tag == this.states[i]) {
+          this.state = tag;
+          return;
         }
-        break;
-      case states.CLOSING:
-        tag.push(char);
-
-        if (!expect(char, ">")) break;
-        if (handler.end(tag)) {
-          handler.body(buf);
-          buf = [];
-          state = states.OUT;
-        } else {
-          buf.push.apply(buf, tag);
-          state = states.BODY;
-        }
-        tag = [];
-        break;
-      default:
-        throw invalidState;
-    }
+      }
+      this.buf.push('<' + tag);
+      for (var i = 0, len = attrs.length; i < len; i++) {
+        this.buf.push(
+          ' ' + attrs[i].name + '="' +
+          (attrs[i].value || '').replace(/"/g, '&#34;') + '"'
+        );
+      }
+      this.buf.push((unary ? '/' : '') + '>');
+    },
+    end: function(tag) {
+      if (this.state == tag) {
+        ctx[this.state] = this.buf.join('');
+        this.buf = [];
+        this.state = null;
+      } else {
+        this.buf.push('</' + tag + '>');
+      }
+    },
+    chars: function(text) {
+      this.buf.push(text);
+    },
   });
+  return ctx;
 }
 
 function compile(context) {
-  var stream = fs.createWriteStream(context.output, {encoding: "utf8"});
+  var stream = fs.createWriteStream(context.output, {encoding: 'utf8'});
+
   stream.once('open', function(_) {
     context.inputs.forEach(function(input) {
-      fs.readFile(input, {encoding: "utf8"}, function (err, data) {
+      fs.readFile(input, {encoding: 'utf8'}, function (err, data) {
         if (err) throw err;
-
-        var handler = new Handler();
-        parse(data, handler);
-        console.log(handler.nodes);
-//        stream.write(data);
+        var ctx = parse(data);
+        console.log(ctx);
       });
     });
   });
@@ -192,7 +133,7 @@ try {
   if (e == help) {
     console.log(help);
   } else {
-    console.error("ERROR:", e, "\n");
+    console.error('ERROR:', e, '\n');
     console.error(help);
   }
 }
